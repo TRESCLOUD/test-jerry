6343fe32377fbdf82b9521266e25695298b9b403
JerryRivera1994 <jerry_rivera1994@hotmail.com>
2025-06-25 10:11:10 -0500

    [ADD][JR] Se agrega modulo donde se realizara el desarrollo de prueba

---
+++ trescloud_test_jerry/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo and Trescloud. See LICENSE file for full copyright and licensing details.
+
+{
+	'name': 'Test Jerry',
+	'version': '1.0',
+	'category': '',
+	'description': '''
+ 	Personalizaciones 
+  		
+	Autores:
+	- Jerry Rivera
+	''',
+	'author': 'TRESCLOUD',
+	'maintainer': 'TRESCLOUD',
+	'website': 'https://www.trescloud.com',
+	'summary': '',
+	'license': 'OEEL-1',
+	'depends': [
+
+	],
+	'data': [
+		# security
+		# data
+		# views
+		# wizards
+	],
+	'application': False,
+	'installable': True,
+	'auto_install': False,
+}
